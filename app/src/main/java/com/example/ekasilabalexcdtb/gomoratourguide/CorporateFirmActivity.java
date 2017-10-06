b1dc01805f1d870ff61a261ade419eefbbc442ce
Difference-Chauke <dchauke9@gmail.com>
2017-10-06 14:48:57 +0200

    Initial commit

---
+++ app/src/main/java/com/example/ekasilabalexcdtb/gomoratourguide/CorporateFirmActivity.java
@@ -0,0 +1,13 @@
+package com.example.ekasilabalexcdtb.gomoratourguide;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CorporateFirmActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_corporate_firm);
+    }
+}
